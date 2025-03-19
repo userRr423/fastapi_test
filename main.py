f977b3295a836f558606a66c4e27b599aaa37677
userRr423 <misterinkognito67@gmail.com>
2025-03-19 18:26:35 +0300

    first comit

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/users")
+async def get_users():
+    return [{"id":1, "name":"Artem"}]
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8080)
