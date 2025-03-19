534e573431be73296b9208bc3981228ee757e261
userRr423 <117460437+userRr423@users.noreply.github.com>
2025-03-19 20:11:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ async def get_users():
     return [{"id":1, "name":"Artem"}]
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
